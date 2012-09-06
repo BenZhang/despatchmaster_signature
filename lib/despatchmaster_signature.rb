4cd7125e638206596c6dec2fd8c3b96964e5e289
BenZhang <bzbnhang@gmail.com>
2012-09-06 22:41:14 +0930

    fix indent

--- lib/despatchmaster_signature.rb
+++ lib/despatchmaster_signature.rb
@@ -4,42 +4,42 @@ include Magick
 
 module DespatchmasterSignature
 
-	class Drawer
-  	attr_reader :length
-  	attr_reader :values
+  class Drawer
+    attr_reader :length
+    attr_reader :values
 
-		def initialize(data, width = 153, height = 55)
-			@width  = width
-			@height = height
-			if data.is_a? String
-				@values = parse(data)
-			elsif data.is_a? Array
-				@values = data
-				@length = data.length
-			end
-		end
+    def initialize(data, width = 153, height = 55)
+      @width  = width
+      @height = height
+      if data.is_a? String
+        @values = parse(data)
+      elsif data.is_a? Array
+        @values = data
+        @length = data.length
+      end
+    end
 
-		def draw(path)
-			rvg = RVG.new(@width, @height) do |canvas|
-				canvas.background_fill = 'white'
-				(0...@length).each do |i|
-					# if the pen left or is about to left, then stop 
-					if (@values[i][0] == -1 or @values[i][1] == -1) or (i + 1 < @length and (@values[i+1][0] == -1 or @values[i+1][1] == -1))
-						next
-					else
-						canvas.line @values[i][0], @values[i][1], @values[i+1][0], @values[i+1][1]
-					end
-				end
-			end
-			rvg.draw.write(path)
-		end
+    def draw(path)
+      rvg = RVG.new(@width, @height) do |canvas|
+        canvas.background_fill = 'white'
+        (0...@length).each do |i|
+          # if the pen left or is about to left, then stop 
+          if (@values[i][0] == -1 or @values[i][1] == -1) or (i + 1 < @length and (@values[i+1][0] == -1 or @values[i+1][1] == -1))
+            next
+          else
+            canvas.line @values[i][0], @values[i][1], @values[i+1][0], @values[i+1][1]
+          end
+        end
+      end
+      rvg.draw.write(path)
+    end
 
-		private
+    private
 
-		def parse(str)
-			str =~ /(\d+);{(.*)}/
-			@length = $1.to_i
-			@values = $2.split(';').collect{|v| v.split(',').collect{|vc| vc.to_i } }
-		end
-	end
+    def parse(str)
+      str =~ /(\d+);{(.*)}/
+      @length = $1.to_i
+      @values = $2.split(';').collect{|v| v.split(',').collect{|vc| vc.to_i } }
+    end
+  end
 end
--- spec/despatchmaster_signature_spec.rb
+++ spec/despatchmaster_signature_spec.rb
@@ -1,26 +1,23 @@
 require 'spec_helper'
 
 describe DespatchmasterSignature do
-	before do
-		
-	end
 
-	describe '.new' do
-		it 'should be able to parse String' do
-			des = DespatchmasterSignature::Drawer.new "7;{16,20;15,23;11,31;10,33;10,24;11,29;13,31;-1,-1}"
-			des.length.should == 7
-			des.values.should == [[16, 20], [15, 23], [11, 31], [10, 33], [10, 24], [11, 29], [13, 31], [-1, -1]]
-		end
-		
-		it 'should be able to read array' do
-			des = DespatchmasterSignature::Drawer.new [[1,1], [2,2], [3,3]]
-			des.length.should == 3
-		end
-	end
+  describe '.new' do
+    it 'should be able to parse String' do
+      des = DespatchmasterSignature::Drawer.new "7;{16,20;15,23;11,31;10,33;10,24;11,29;13,31;-1,-1}"
+      des.length.should == 7
+      des.values.should == [[16, 20], [15, 23], [11, 31], [10, 33], [10, 24], [11, 29], [13, 31], [-1, -1]]
+    end
+    
+    it 'should be able to read array' do
+      des = DespatchmasterSignature::Drawer.new [[1,1], [2,2], [3,3]]
+      des.length.should == 3
+    end
+  end
 
-	describe '#draw' do
-		des = DespatchmasterSignature::Drawer.new "11;{16,20;15,23;13,25;11,31;10,33;10,24;11,29;13,31;-1,-1;80,9;81,7;82,6;80,10;76,19;71,26;67,34;63,38;60,42;60,42;-1,-1}"
-		des.draw('duck.png')
-	end
+  describe '#draw' do
+    des = DespatchmasterSignature::Drawer.new "11;{16,20;15,23;13,25;11,31;10,33;10,24;11,29;13,31;-1,-1;80,9;81,7;82,6;80,10;76,19;71,26;67,34;63,38;60,42;60,42;-1,-1}"
+    des.draw('duck.png')
+  end
 
 end
